ae57b5de90a2b445bd7dd71076523324b56d74ba
gerald <gerald@gmail.com>
2025-09-19 19:15:09 -0600

    quitando linea comentada y espacios innecesarios soy el q no sabe de git xd

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -7,9 +7,9 @@ import Maps from "./components/maps/Maps"; // no funciona con llaves
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<DefaultLayout />}>
-        <Route index element={<HomePage />} />
-        {/*   <Route path="Quiz" element={<Quiz />} />   */}
+      <Route path="/" element={<DefaultLayout/>}>
+        <Route index element={<HomePage/>} />
+          <Route path="Quiz" element={<Quiz/>} /> 
 
         <Route path="mapa" element={<Maps />} />
       </Route>
