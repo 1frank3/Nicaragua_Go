25b67bd7e26cf28ddd88bbcc3474f755248abc2b
1frank3 <mercadopadillageraldinefrancis@gmail.com>
2025-09-27 16:27:42 -0600

    subo header y footer con nuevo estilo

--- frontend/src/components/core/Header.jsx
+++ frontend/src/components/core/Header.jsx
@@ -1,46 +1,18 @@
-import { Link } from "react-router-dom";
-// import logo from "../assets/logo.png";
+import { useState } from "react";
+import { Link } from "react-router-dom"
+import { FaBars } from "react-icons/fa"
+import "../../styles/header.css"
 import logo from "../../assets/img/logo.png"
 
-export  function Header() {
-  return (
-    <header className="bg-blue-800 text-white">
-      <div className="max-w-7xl mx-auto flex items-center justify-between px-6 py-4">
-       
-        <div className="flex items-center gap-3">
-          <img src={logo} alt="Memoria Viva Logo" className="h-10 w-auto" />
-          <h1 className="text-xl font-bold">
-            Memoria <span className="text-pink-500">Viva</span>{" "}
-            <span className="text-blue-300">Nicaragua</span>
-          </h1>
-        </div>
+export function Header() {
+  
 
-      
-        <nav>
-          <ul className="flex space-x-6 text-sm font-medium">
-            <li>
-              <Link to="/" className="hover:text-yellow-400 transition">
-                Inicio
-              </Link>
-            </li>
-            <li>
-              <Link to="/mapa" className="hover:text-yellow-400 transition">
-                Mapa
-              </Link>
-            </li>
-            <li>
-              <Link to="/quiz" className="hover:text-yellow-400 transition">
-                Quiz
-              </Link>
-            </li>
-            <li>
-              <Link to="/contacto" className="hover:text-yellow-400 transition">
-                Contacto
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+  return (
+    <header className="header">
+      {/* Logo */}
+      <div className="logo">
+        <img src={logo} alt="NicaraguaGo Logo" />
+        NicaraguaGo</div>
     </header>
   );
 }
--- frontend/src/components/core/footer.jsx
+++ frontend/src/components/core/footer.jsx
@@ -2,28 +2,28 @@ import { FaFacebook, FaInstagram, FaWhatsapp, FaYoutube, FaTiktok } from "react-
 
 export  function Footer() {
   return (
-    <footer className="bg-blue-900 text-white mt-10">
+    <footer className="bg-[#012840] text-white mt-10">
       <div className="max-w-7xl mx-auto px-6 py-6 flex flex-col md:flex-row items-center justify-between">
         {/* Texto */}
         <p className="text-sm text-center md:text-left">
-          ©HACKATON NICARAGUA 2025
+          ©Nicaragua Go 2025
         </p>
 
         {/* Redes sociales */}
         <div className="flex space-x-5 mt-3 md:mt-0">
-          <a href="#" className="hover:text-yellow-400 transition">
+          <a href="#" className="text-yellow-400 transition">
             <FaYoutube size={20} />
           </a>
-          <a href="#" className="hover:text-yellow-400 transition">
+          <a href="#" className="text-yellow-400 transition">
             <FaFacebook size={20} />
           </a>
-          <a href="#" className="hover:text-yellow-400 transition">
+          <a href="#" className="text-yellow-400 transition">
             <FaInstagram size={20} />
           </a>
-          <a href="#" className="hover:text-yellow-400 transition">
+          <a href="#" className="text-yellow-400 transition">
             <FaWhatsapp size={20} />
           </a>
-          <a href="#" className="hover:text-yellow-400 transition">
+          <a href="#" className="text-yellow-400 transition">
             <FaTiktok size={20} />
           </a>
         </div>
